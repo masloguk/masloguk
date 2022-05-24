ab2eb48f90f337ee0d13601a49394a0fd429e909
«Artyom» <masloguk@mail.ru>
2022-05-24 16:40:37 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,10 @@
+def random_predict(number:int=1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
